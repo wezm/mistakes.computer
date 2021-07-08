7080d67552268649217677246101ff457d52c81a
Wesley Moore <wes@wezm.net>
2021-07-09 09:31:37 +1000

    Add footer; work around static file issues

--- src/index.tsx
+++ src/index.tsx
@@ -31,11 +31,14 @@ const Mistakes = function() {
             <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <link rel="icon" href="favicon.png" type="image/png"/>
-            <link rel="stylesheet" href="style.css" />
             <title>mistakes.computer</title>
         </head>
-        <body>
+        <body style="text-align: center; font-family: sans-serif;">
             <h1>{mistakeText()}</h1>
+            <footer style="font-size: 9pt; position: absolute; bottom: 0; left: 10px;">
+                <p>A silly project by <a href="https://www.wezm.net/">wezm</a> to try out <a href="https://deno.com/deploy">Deno Deploy</a>{". "}
+                    <a href="https://github.com/wezm/mistakes.computer">Source on GitHub</a>.</p>
+            </footer>
         </body>
         </html>
     );
