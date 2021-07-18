3b345a6bc77f3bf1a40571eab49a2e76a58d3c7e
Wesley Moore <wes@wezm.net>
2021-07-18 12:18:51 +1000

    Format index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,39 +8,39 @@ import {
 } from "https://deno.land/x/sift@0.3.4/mod.ts";
 import MISTAKES from "./mistakes.ts";
 
-function random(min: number, max: number) : number {
+function random(min: number, max: number): number {
     return Math.floor(
         (Math.random() * (max - min + 1)) + min
     );
 }
 
-function pick<T>(array: Array<T>) : T {
+function pick<T>(array: Array<T>): T {
     const i = random(0, array.length - 1);
     return array[i];
 }
 
-function mistakeText() : string {
+function mistakeText(): string {
     const mistake = pick(MISTAKES);
     return `${mistake[0]} ${mistake[1]} a mistake`
 }
 
-const Mistakes = function() {
+const Mistakes = function () {
     return (
         <html>
-        <head>
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link rel="stylesheet" href="style.css" />
-            <link rel="icon" href="favicon.png" type="image/png" />
-            <title>mistakes.computer</title>
-        </head>
-        <body>
-            <h1>{mistakeText()}</h1>
-            <footer style="font-size: 9pt; position: absolute; bottom: 0; left: 10px;">
-                <p>A silly project by <a href="https://www.wezm.net/">wezm</a> to try out <a href="https://deno.com/deploy">Deno Deploy</a>{". "}
-                    <a href="https://github.com/wezm/mistakes.computer">Source on GitHub</a>.</p>
-            </footer>
-        </body>
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="stylesheet" href="style.css" />
+                <link rel="icon" href="favicon.png" type="image/png" />
+                <title>mistakes.computer</title>
+            </head>
+            <body>
+                <h1>{mistakeText()}</h1>
+                <footer style="font-size: 9pt; position: absolute; bottom: 0; left: 10px;">
+                    <p>A silly project by <a href="https://www.wezm.net/">wezm</a> to try out <a href="https://deno.com/deploy">Deno Deploy</a>{". "}
+                        <a href="https://github.com/wezm/mistakes.computer">Source on GitHub</a>.</p>
+                </footer>
+            </body>
         </html>
     );
 }
@@ -64,7 +64,7 @@ async function slashCommand(request: Request) {
         );
     }
 
-    return json({"text": mistakeText()});
+    return json({ "text": mistakeText() });
 }
 
 function verifyToken(request: Request): boolean {
@@ -74,9 +74,9 @@ function verifyToken(request: Request): boolean {
 }
 
 serve({
-    "/": () => jsx(<Mistakes/>),
+    "/": () => jsx(<Mistakes />),
     "/slash": slashCommand,
-    "/mistake.json": () => json({mistake: mistakeText()}),
+    "/mistake.json": () => json({ mistake: mistakeText() }),
     "/favicon.png": serveStatic("favicon.png", { baseUrl: import.meta.url }),
     "/style.css": serveStatic("style.css", { baseUrl: import.meta.url }),
 })
